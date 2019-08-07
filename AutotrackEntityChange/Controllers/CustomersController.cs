b0512d60f93eb4b289829b5df8bd08e7a256bb58
benktesh <benktesh@yahoo.com>
2019-08-07 15:22:59 -0500

    Adds mechanism to add/edit/remove customer through a controller

---
+++ AutotrackEntityChange/Controllers/CustomersController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutotrackEntityChange.DBContext;
+using Newtonsoft.Json;
+
+namespace AutotrackEntityChange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var task = await _context.Customers.ToListAsync();
+            return Ok(await _context.Customers.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+       
+        // POST: Customers/Create
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([Bind("AccountNumber,Name,CreatedDate,ModifiedDate,LastModifiedBy,IsInactive")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.Id = Guid.NewGuid();
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(customer);
+        }
+
+
+        // POST: Customers/Edit/5
+        [HttpPost("edit/{id}")]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,AccountNumber,Name,CreatedDate,ModifiedDate,LastModifiedBy,IsInactive")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost("delete/{id}")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(Guid id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
